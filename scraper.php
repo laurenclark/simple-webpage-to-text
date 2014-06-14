c99eaa8b2ab78d550f4db9af38c5348ed65f6538
Lauren <lauren.c4231@gmail.com>
2014-06-14 23:20:13 +0100

    Remove crap.

--- scraper.php
+++ scraper.php
@@ -40,48 +40,10 @@ function curl_get_file_contents($URL)
 
   $urls = array(
 
-  	'http://www.gtduk.com/',
-  	'http://www.gtduk.com/about-us.php',
-  	'http://www.gtduk.com/case-studies/',
-  	'http://www.gtduk.com/contact-us.php',
-  	'http://www.gtduk.com/databases/',
-  	'http://www.gtduk.com/databases/case-study-1.php',
-  	'http://www.gtduk.com/databases/case-study-2.php',
-  	'http://www.gtduk.com/databases/case-study-3.php',
-  	'http://www.gtduk.com/databases/case-study-4.php',
-  	'http://www.gtduk.com/databases/index.php',
-  	'http://www.gtduk.com/identity/',
-  	'http://www.gtduk.com/identity/branding.php',
-  	'http://www.gtduk.com/identity/copywriting.php',
-  	'http://www.gtduk.com/identity/logo-design.php',
-  	'http://www.gtduk.com/identity/packaging.php',
-  	'http://www.gtduk.com/multimedia/',
-  	'http://www.gtduk.com/multimedia/video-packages.php',
-  	'http://www.gtduk.com/multimedia/video-production.php',
-  	'http://www.gtduk.com/news/missing-andy-app.php',
-  	'http://www.gtduk.com/news/network-magazine-8th-birthday.php',
-  	'http://www.gtduk.com/news/nexus-redesign.php',
-  	'http://www.gtduk.com/online/',
-  	'http://www.gtduk.com/online/app-design-development.php',
-  	'http://www.gtduk.com/online/content-management-systems.php',
-  	'http://www.gtduk.com/online/digital-marketing.php',
-  	'http://www.gtduk.com/online/search-engine-optimisation.php',
-  	'http://www.gtduk.com/online/website-design.php',
-  	'http://www.gtduk.com/portfolio/',
-  	'http://www.gtduk.com/portfolio/apps.php',
-  	'http://www.gtduk.com/portfolio/index.php',
-  	'http://www.gtduk.com/portfolio/multimedia.php',
-  	'http://www.gtduk.com/portfolio/offline.php',
-  	'http://www.gtduk.com/portfolio/online.php',
-  	'http://www.gtduk.com/portfolio/printed.php',
-  	'http://www.gtduk.com/printing/',
-  	'http://www.gtduk.com/printing/brochures.php',
-  	'http://www.gtduk.com/printing/promotional-materials.php',
-  	'http://www.gtduk.com/printing/signs.php',
-  	'http://www.gtduk.com/printing/stationery.php',
-  	'http://www.gtduk.com/privacy-policy.php',
-  	'http://www.gtduk.com/terms-conditionas.php',
-  	'http://www.gtduk.com/testimonials.php'
+#list of urls...
+    'http://google.com',
+    'http://laurenclark.io'
+    
   	);
 
 foreach ($urls as $url) { 
